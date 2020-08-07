70637cb85969bd561bdc3a37b671c62f2af0f52a
Eric Sexton <foopis23@gmail.com>
2020-08-07 08:47:11 -0400

    Buildy Some Level, Renamy some stuff, Addy Level Data

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName ="Level Data")]
+public class LevelData : ScriptableObject
+{
+    public string Name;
+    public int ParScore;
+}
